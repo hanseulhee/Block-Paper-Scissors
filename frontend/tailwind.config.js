b662af24693700295cebd286ca60405851030d90
hanseulhee <3021062@gmail.com>
2025-02-23 17:06:25 +0900

    apply tailwind settings

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#AF76F7',
+        secondary: '#3B3FB1',
+        dark: '#11005D',
+        light: '#BBC6FB',
+        accent: '#D58EFC',
+      },
+    },
+  },
+  plugins: [],
+}
