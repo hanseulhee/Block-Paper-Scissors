cb68a00b2ff1168f3aedfdaa91435b8dd78b1b09
hanseulhee <3021062@gmail.com>
2025-02-23 14:00:03 +0900

    apply layout

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import useWindowSize from "../hooks/useWindowSize";
+
+let vh = 0;
+
+function Layout() {
+  const windowSize = useWindowSize();
+
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, [windowSize.height]);
+
+  return (
+    <main className="min-h-[calc(var(--vh,1vh)*100)] max-w-[480px] w-full mx-auto h-screen bg-white pt-10 pb-10">
+      <Outlet />
+    </main>
+  );
+}
+
+export default Layout;
